b5dc2441b0abdec388d501dcecb69edb8978a70f
ProjectRobal <patryk1-70@wp.pl>
2025-06-16 21:14:48 +0200

    I have to fix prod Dockerfile

--- entrypoints/entrypoint.dev.sh
+++ entrypoints/entrypoint.dev.sh
@@ -1,3 +1,19 @@
+echo "Installing dependencies"
+
+apt update
+
+apt install gettext -y
+
+cat '/dep/packages.txt' | envsubst | xargs apt -y install
+
+rosdep update
+
+pip3 install --upgrade pip 
+
+rosdep install --from-paths /app --ignore-src  -r -y -q
+
+find /app -name "dependencies.txt" -exec pip3 install -r {} \;
+
 echo "Hello Dev"
 
 sleep infinity
